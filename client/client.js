375b4f021710fcd30d0da9abc1d1fdb9b8ef227f
Tamarind Stafford <joytamarind@gmail.com>
2020-02-13 13:28:33 -0600

    Log form data through express routes

--- client/client.js
+++ client/client.js
@@ -1,3 +1,29 @@
 /*  Select Elements */
 const form = document.querySelector(".form");
 const buttonSubmit = document.querySelector("#buttonSubmit");
+const messageInput = document.querySelector("#messageInput");
+const nameInput = document.querySelector("#nameInput");
+const POST_URL = "http://localhost:3000/userpost";
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const username = formData.get("userName");
+  const userMessage = formData.get("userMessage");
+  const tweet = {
+    username,
+    userMessage
+  };
+
+  fetch(POST_URL, {
+    method: "POST",
+    body: JSON.stringify(tweet),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+
+  messageInput.value = "";
+  nameInput.value = "";
+});
--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 /* Initialize variables */
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
 
 /* Middleware */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 /* Routes */
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello!"
+  });
+});
+
+app.post("/userpost", (req, res) => {
+  console.log(req.body);
+});
 
 /* Listen */
 app.listen(3000, () => {

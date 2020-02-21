3a922c84d01bc3764e0009891161c27d3b786820
Tamarind Stafford <joytamarind@gmail.com>
2020-02-20 20:38:03 -0600

    Validate input on the client side

--- client/client.js
+++ client/client.js
@@ -5,10 +5,13 @@ const messageInput = document.querySelector("#messageInput");
 const nameInput = document.querySelector("#nameInput");
 const POST_URL = "http://localhost:3000/userpost";
 const tweetList = document.querySelector(".list");
+const tweetError = document.querySelector(".tweetError");
+const nameError = document.querySelector(".nameError");
 
 form.addEventListener("submit", event => {
   event.preventDefault();
 
+  // Get form data
   const formData = new FormData(form);
   const userName = formData.get("userName");
   const userMessage = formData.get("userMessage");
@@ -17,6 +20,7 @@ form.addEventListener("submit", event => {
     userMessage
   };
 
+  // Fetch form data from server
   fetch(POST_URL, {
     method: "POST",
     body: JSON.stringify(tweet),
@@ -25,18 +29,35 @@ form.addEventListener("submit", event => {
     }
   });
 
-  tweetList.insertAdjacentHTML(
-    "beforeend",
-    `<div class="list-item">
-      <figure class="image is-64x64">
-    <img class="is-rounded" src="https://robohash.org/${userName}.png?bgset=bg1">
-  </figure>
+  // Validate input
+  if (!messageInput.value) {
+    messageInput.classList.add("is-danger");
+    // Display error message
+    tweetError.style.display = "block";
+  } else if (!nameInput.value) {
+    nameInput.classList.add("is-danger");
+    nameError.style.display = "block";
+  } else {
+    // Clear red outline & error messages
+    messageInput.classList.remove("is-danger");
+    nameInput.classList.remove("is-danger");
+    nameError.style.display = "none";
+    tweetError.style.display = "none";
 
-      <h6 class="title is-6">${userName}</h6>
-      <h5 class="subtitle is-5">${userMessage}</h5>
-      </div>`
-  );
-
-  messageInput.value = "";
-  nameInput.value = "";
+    // Render tweet to page if inputs are not empty
+    tweetList.insertAdjacentHTML(
+      "beforeend",
+      `<div class="list-item">
+          <figure class="image is-64x64">
+        <img class="is-rounded" src="https://robohash.org/${userName}.png?bgset=bg1">
+      </figure>
+    
+          <h6 class="title is-5">${userName}</h6>
+          <h5 class="subtitle is-6">${userMessage}</h5>
+          </div>`
+    );
+    // Reset input after submit
+    messageInput.value = "";
+    nameInput.value = "";
+  }
 });
